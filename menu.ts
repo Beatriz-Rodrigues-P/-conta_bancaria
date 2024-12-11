6e80b807919802a4b352e27fe326ba228a30c58b
Beatriz <beatriz.rodrigues04@outlook.com>
2024-12-11 11:27:05 -0300

    Menu e Conta

--- menu.ts
+++ menu.ts
@@ -1,134 +1,110 @@
 import readlinesync=require("readline-sync")
 import {colors} from "./src/util/cores"
-import {Stack} from "./Stack"
+import {Conta} from "./src/model/conta"
 
-let opcao:number
-const contaslista=new Stack<string>()
-let valor:number
+export function main(){
 
-do{
-    menu()
-    opcao=(readlinesync.questionInt("\nDigite a operacao a ser executada: "))
+    let opcao:number
 
-    switch(opcao){
-        case 1:
-            criar()
-        break
-        case 2:
-            listar()
-        break
-        case 3:
-            buscar()
-        break
-        case 4:
-            atualizar()
-        break
-        case 5:
-            apagar()
-        break
-        case 6:
-            sacar()
-        break
-        case 7:
-            depositar()
-        break
-        case 8:
-            transferir()
-        break
-        case 9:
-            console.log("Operação finalizada com sucesso!")
-        break
-    default:
-            console.log(colors.fg.redstrong,"Operação inválida. Insira um valor válido!")
-        break
+    const c1=new Conta(1, 123, 1, "Jonas", 2000000);
+    c1.visualizar()
+    console.log(c1.sacar(100.00))
+    c1.visualizar()
+
+    const c2=new Conta(2, 234, 2, "Andressa", 1000000);
+    c2.visualizar()
+    c2.depositar(100.00)
+    c2.visualizar()
+    
+    while(true){
+        
+        //Menu
+        console.log(colors.fg.whitestrong, colors.bg.cyanbright)
+        console.log("___________________________________________________")
+        console.log("                                                   ")
+        console.log("                    BANCO BRP                      ")
+        console.log("___________________________________________________")
+        console.log("                                                   ")
+        console.log("    1 - Criar conta                                ")
+        console.log("    2 - Listar todas as contas                     ")
+        console.log("    3 - Buscar uma conta                           ")
+        console.log("    4 - Atualizar dados da conta                   ")
+        console.log("    5 - Apagar conta bancaria                      ")
+        console.log("    6 - Sacar                                      ")
+        console.log("    7 - Depositar                                  ")
+        console.log("    8 - Realizar transferência bancaria            ")
+        console.log("    9 - Sair do programa                           ")
+        console.log("___________________________________________________")
+        console.log(colors.reset)
+
+        console.log("Insira a operação desejada: ")
+        opcao=readlinesync.questionInt("");
+
+        if(opcao==9){
+            console.log(colors.fg.whitestrong,"Banco BRP proporcionando segurança e inovação!")
+            sobre();
+            console.log(colors.reset)
+            process.exit()
+        }
+
+        switch(opcao){
+            case 1:
+                console.log(colors.fg.white, "Criar uma conta")
+                keyPress()
+                break
+            case 2:
+                console.log(colors.fg.white, "Listar as contas inseridas")
+                keyPress()
+                break
+            case 3:
+                console.log(colors.fg.white, "Buscar uma conta no sistema")
+                keyPress()
+                break
+            case 4:
+                console.log(colors.fg.white, "Atualizar uma conta")
+                keyPress()
+                break
+            case 5:
+                console.log(colors.fg.white, "Apagar uma conta")
+                keyPress()
+                break
+            case 6:
+                console.log(colors.fg.white, "Sacar um valor da conta")
+                keyPress()
+                break
+            case 7:
+                console.log(colors.fg.white, "Depositar um valor na conta")
+                keyPress()
+                break
+            case 8:
+                console.log(colors.fg.white, "Transferir um valor de uma conta para outra")
+                keyPress()
+                break
+            case 9:
+                console.log(colors.fg.white, "Operação finalizada com sucesso!")
+                keyPress()
+                break
+        default:
+                console.log(colors.fg.redstrong,"Operação inválida. Insira um valor válido!")
+            break
+        }
+        
     }
-}while(true)
 
-//Menu
-function menu():void{
-console.log(colors.fg.whitestrong, colors.bg.cyanbright)
-console.log("___________________________________________________")
-console.log("                                                   ")
-console.log("                    BANCO BRP                      ")
-console.log("___________________________________________________")
-console.log("                                                   ")
-console.log("    1 - Criar conta                                ")
-console.log("    2 - Listar todas as contas                     ")
-console.log("    3 - Buscar uma conta                           ")
-console.log("    4 - Atualizar dados da conta                   ")
-console.log("    5 - Apagar conta bancaria                      ")
-console.log("    6 - Sacar                                      ")
-console.log("    7 - Depositar                                  ")
-console.log("    8 - Realizar transferência bancaria            ")
-console.log("    9 - Sair do programa                           ")
-console.log("___________________________________________________")
-}
-//Digitar os dados solicitados
-function criar():void{
-    const conta=readlinesync.question("Criar conta: ")
-    contaslista.push(conta)
-    console.log(`Conta ${conta} criada com sucesso!`)
-}
-//Listar todas as contas que foram criadas
-function listar():void{
-    console.log("Lista de contas bancarias: ")
-    if(contaslista.isEmpty()){
-        console.log("Não há contas criadas!")
-    }else{contaslista.printStack()}
-}
-//Buscar uma conta específica
-function buscar():void{
-    const buscaconta=readlinesync.question("Buscar uma conta bancária: ")
-    if(contaslista.contains((num)=>num===buscaconta)){
-    console.log(`A conta ${buscaconta} está no sistema.`)
-}else{console.log(`A conta ${buscaconta} não existe no sistema`)}
-}
-//Atualizar os dados de uma conta que buscou
-function atualizar():void{
-    const dados=readlinesync.question("Insira a conta que deseja atualizar: ")
-    const listadados= contaslista.findIndex((contab)=>contab===dados)
-    if (listadados!==-1){ 
-        const dadosatu=readlinesync.question("Numero atualizado: ")
-        contaslista.items[listadados]=dadosatu
-        console.log(`Conta ${dados} atualizada com sucesso. O novo número é: ${dadosatu}.`) 
-        } else {
-        console.log("Esta conta não é válida!")}
-}
-//Apagar uma conta 
-function apagar():void{
-    const dados=readlinesync.question("Insira a conta que deseja apagar: ")
-    const listadados=contaslista.findIndex((contab)=>conta===dados)
-    if (listadados!=-1){
-        constaslista.remove(listadados)
-        console.log(`A conta ${dados} foi excluida!`)
-    }else{
-        console.log("Conta ${dados} não foi rastreada!")
-    }
-}
-//Sacar o valor de uma conta
-function sacar():void{
-    const dados=readlinesync.question("Insira a conta: ")
-    const listadados = contaslista.findIndex((contab)=>contab===dados)
-    if (listadados!==-1){
-        const valor=readlinesync.questionFloat("Insira o valor que deseja sacar: ")
-        console.log(`R$${valor} foram sacados da conta ${listadados}.`)
-    } else { console.log("Conta não encontrada.")}
-}
-//Depositar o valor em uma conta
-function depositar():void{
-    const dados=readlinesync.question("Número da conta: ")
-    const indice = contaslista.findIndex((contab)=>contab===dados)
-    if (indice !== -1){
-        const valor=readlinesync.questionFloat("Valor que deseja depositar: ")
-        console.log(`R$${valor} foi depositado na conta ${dados}.`)
-    }else{console.log("A conta não foi rastreada.")
-}
-//Transferir um valor de uma conta para outra
-function transferir():void{
-    const origem=readlinesync.question("Insira a conta de origem: ")
-    const destino=readlinesync.question("Insira a conta de destino: ")
-    const valor=readlinesync.questionFloat("Valor que deseja transferir: ")
-    const listao=contaslista.findIndex()
 }
 
-process.exit(0)
+function sobre():void{
+    console.log("____________________________________________________")
+    console.log("                                                    ")
+    console.log("Projeto Desenvolvido por: Beatriz Rodrigues de Paula");
+    console.log("   Generation Brasil - generation@generation.org    ");
+    console.log("            github.com/conteudoGeneration           ");
+    console.log("____________________________________________________");
+}
+
+function keyPress():void{
+    console.log(colors.reset,"")
+    console.log("\nPressione enter para continuar!")
+    readlinesync.prompt()
+}
+main()
---
+++ src/model/conta.ts
@@ -0,0 +1,104 @@
+import { isNumberObject } from "util/types"
+
+export class Conta{
+
+    //Modelo de dados
+    private _numero: number;
+    private _agencia: number;
+    private _tipo:number;
+    private _titular:string;
+    private _saldo:number;
+
+    constructor(numero: number, agencia: number, tipo:number, titular: string, saldo:number){
+        this._numero=numero
+        this._agencia=agencia
+        this._tipo=tipo
+        this._titular=titular
+        this._saldo=saldo
+    }
+
+    //Gerar os metodos Getter's e Setter's
+	public get numero(): number {
+		return this._numero;
+	}
+
+	public get agencia(): number {
+		return this._agencia;
+	}
+
+	public get tipo(): number {
+		return this._tipo;
+	}
+
+
+	public get titular(): string {
+		return this._titular;
+	}
+
+
+	public get saldo(): number {
+		return this._saldo;
+	}
+
+	public set numero(value: number) {
+		this._numero = value;
+	}
+
+
+	public set agencia(value: number) {
+		this._agencia = value;
+	}
+
+
+	public set tipo(value: number) {
+		this._tipo = value;
+	}
+
+
+	public set titular(value: string) {
+		this._titular = value;
+	}
+
+	public set saldo(value: number) {
+		this._saldo = value;
+	}
+    public sacar(valor:number):boolean{
+        if(valor>this._saldo){
+            console.log("Saldo insuficiente!");
+            return false;
+        }
+        this._saldo-=valor
+        return true;
+    }
+
+    public depositar(valor:number){
+        this._saldo+=valor;
+    }
+
+    public visualizar(){
+        let tipo:string;
+    
+        switch(this._tipo){
+            case 1:
+                tipo="Conta corrente"
+            break
+            case 2:
+                tipo="Tipo poupança"
+            break
+            case 3:
+                tipo="Tipo inválido"
+            break
+    }
+
+        console.log("________________________________")
+        console.log("Dados da conta")
+        console.log("________________________________")
+        console.log(`Número da conta: ${this._numero}`)
+        console.log(`Número da agência: ${this._agencia}`)
+        console.log(`Tipo da conta: ${this._tipo}`)
+        console.log(`Titular da conta: ${this._titular}`)
+        console.log(`Saldo da conta: ${this._saldo}`)
+        
+    }
+	
+}
